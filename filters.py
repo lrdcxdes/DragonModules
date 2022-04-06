9c1392e6f631e9bcff4ce6cbb23b73ba7d2de40f
LORD_CODE <lordgrief176@gmail.com>
2022-04-06 20:53:44 +0300

    fixed modules_help in dice.py

---
+++ filters.py
@@ -0,0 +1,129 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# noinspection PyUnresolvedReferences
+from utils.misc import modules_help, prefix
+from utils.scripts import format_exc
+
+# noinspection PyUnresolvedReferences
+from utils.db import db
+
+
+all_filters_ = {i["word"]: i["answer"] for i in db.get_collection('lordcodes.filters').values()}
+
+
+def all_filters():
+    return all_filters_
+
+
+async def contains_filter(_, __, m):
+    return m.text and m.text.lower() in all_filters().keys()
+
+
+contains = filters.create(contains_filter)
+
+
+@Client.on_message(contains)
+async def filters_main_handler(client: Client, message: Message):
+    return await message.reply(all_filters()[message.text.lower()])
+
+
+@Client.on_message(filters.command(["filter"], prefix) & filters.me)
+async def filter_handler(client: Client, message: Message):
+    try:
+        if len(message.text.split()) < 3:
+            return await message.edit(
+                        f"<b>Usage</b>: <code>{prefix}filter [name] [text] (Reply required)</code>"
+                    )
+        name = message.text.split(maxsplit=2)[1]
+        if db.get('lordcodes.filters', f'filter_{name}'):
+            return await message.edit(
+                        f"<b>Filter</b> <code>{name}</code> already exists."
+                    )
+        text = message.text.split(name)[1].strip()
+        if not message.reply_to_message:
+            return await message.edit('<b>Reply to message</b> please.')
+        elif message.reply_to_message.text:
+            word = message.reply_to_message.text.lower()
+        elif message.reply_to_message.caption:
+            word = message.reply_to_message.caption.lower()
+        else:
+            return await message.edit('<b>Reply to TEXT/CAPTION message</b> please.')
+
+        db.set('lordcodes.filters', f'filter_{name}', {'word': word,
+                                                       'answer': text})
+        all_filters_[word] = text
+        return await message.edit(
+                f"<b>Filter</b> <code>{name}</code> has been added."
+            )
+    except Exception as e:
+        return await message.edit(format_exc(e))
+
+
+@Client.on_message(filters.command(["filters"], prefix) & filters.me)
+async def filters_handler(client: Client, message: Message):
+    try:
+        text = ''
+        for index, a in enumerate(db.get_collection('lordcodes.filters').items(), start=1):
+            key, item = a
+            if key[:6] == 'filter':
+                key = key[7:].replace('<', '').replace('>', '')
+                answer = item['answer'].replace('<', '').replace('>', '')
+                text += f"{index}. <code>{key}</code> — <code>{answer}</code>\n"
+        text = f"<b>Filters</b>:\n\n" \
+               f"{text}"
+        text = text[:4096]
+        return await message.edit(text)
+    except Exception as e:
+        return await message.edit(format_exc(e))
+
+
+@Client.on_message(filters.command(["filterdel", "fdel"], prefix) & filters.me)
+async def filter_del_handler(client: Client, message: Message):
+    try:
+        if len(message.text.split()) < 2:
+            return await message.edit(
+                        f"<b>Usage</b>: <code>{prefix}fdel [name]</code>"
+                    )
+        name = message.text.split(maxsplit=1)[1]
+        value = db.get('lordcodes.filters', f'filter_{name}')
+        if not value:
+            return await message.edit(
+                        f"<b>Filter</b> <code>{name}</code> doesn't exists."
+                    )
+        try:
+            del all_filters_[value['word']]
+        except:
+            pass
+        db.remove('lordcodes.filters', f'filter_{name}')
+        return await message.edit(
+                f"<b>Filter</b> <code>{name}</code> has been deleted."
+            )
+    except Exception as e:
+        return await message.edit(format_exc(e))
+
+
+@Client.on_message(filters.command(["fsearch"], prefix) & filters.me)
+async def filter_search_handler(client: Client, message: Message):
+    try:
+        if len(message.text.split()) < 2:
+            return await message.edit(
+                        f"<b>Usage</b>: <code>{prefix}fsearch [name]</code>"
+                    )
+        name = message.text.split(maxsplit=1)[1]
+        value = db.get('lordcodes.filters', f'filter_{name}', False)
+        if not value:
+            return await message.edit(f'<b>Filter</b> <code>{name}</code> doesn\'t exists.')
+        return await message.edit(f'<b>Filter</b> <code>{name}</code>:\n<b>Trigger</b>: <code>{value["word"]}</code'
+                                  f'>\n<b>Answer</b> '
+                                  f': <code>{value["answer"]}</code>')
+    except Exception as e:
+        return await message.edit(format_exc(e))
+
+
+modules_help['filters'] = {
+    'filter [name] [text]': 'Create filter (Reply required)',
+    'filters': 'List of all triggers',
+    'fdel [name]': 'Delete filter by name',
+    'fsearch [name]': 'Info filter by name'
+}
---
+++ ytdl.py
@@ -0,0 +1,98 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from utils.misc import modules_help, prefix
+from utils.scripts import format_exc, import_library
+import os
+from io import BytesIO
+
+
+youtube_dl = import_library("youtube_dl")
+YoutubeDL = youtube_dl.YoutubeDL
+DownloadError = youtube_dl.utils.DownloadError
+ContentTooShortError = youtube_dl.utils.ContentTooShortError
+ExtractorError = youtube_dl.utils.ExtractorError
+GeoRestrictedError = youtube_dl.utils.GeoRestrictedError
+MaxDownloadsReached = youtube_dl.utils.MaxDownloadsReached
+PostProcessingError = youtube_dl.utils.PostProcessingError
+UnavailableVideoError = youtube_dl.utils.UnavailableVideoError
+XAttrMetadataError = youtube_dl.utils.XAttrMetadataError
+
+strings = {
+    "name": "Youtube-Dl",
+    "preparing": "<b>[YouTube-Dl]</b> Preparing...",
+    "downloading": "<b>[YouTube-Dl]</b> Downloading...",
+    "working": "<b>[YouTube-Dl]</b> Working...",
+    "exporting": "<b>[YouTube-Dl]</b> Exporting...",
+    "reply": "<b>[YouTube-Dl]</b> No link!",
+    "noargs": "<b>[YouTube-Dl]</b> No args!",
+    "content_too_short": "<b>[YouTube-Dl]</b> Downloading content too short!",
+    "geoban": "<b>[YouTube-Dl]</b> The video is not available for your geographical location due to geographical "
+              "restrictions set by the website!",
+    "maxdlserr": '<b>[YouTube-Dl]</b> The download limit is as follows: " oh ahah"',
+    "pperr": "<b>[YouTube-Dl]</b> Error in post-processing!",
+    "noformat": "<b>[YouTube-Dl]</b> Media is not available in the requested format",
+    "xameerr": "<b>[YouTube-Dl]</b> {0.code}: {0.msg}\n{0.reason}",
+    "exporterr": "<b>[YouTube-Dl]</b> Error when exporting video",
+    "err": "<b>[YouTube-Dl]</b> {}"
+}
+
+
+@Client.on_message(filters.command(["ytdl", "dlyt"], prefix) & filters.me)
+async def ytdl_handler(client: Client, message: Message):
+    try:
+        url = message.command[1]
+    except IndexError:
+        return await message.edit(strings["noargs"])
+    await message.edit(strings["preparing"])
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [
+            {"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}
+        ],
+        "outtmpl": "downloads/%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    await message.edit(strings["downloading"])
+    try:
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        return await message.edit(strings["err"].format(DE))
+    except ContentTooShortError:
+        return await message.edit(strings["content_too_short"])
+    except GeoRestrictedError:
+        return await message.edit(strings["geoban"])
+    except MaxDownloadsReached:
+        return await message.edit(strings["maxdlserr"])
+    except PostProcessingError:
+        return await message.edit(strings["pperr"])
+    except UnavailableVideoError:
+        return await message.edit(strings["noformat"])
+    except XAttrMetadataError as XAME:
+        return await message.edit(strings["xameerr"].format(XAME))
+    except ExtractorError:
+        return await message.edit(strings["exporterr"])
+    except Exception as e:
+        return await message.edit('<b>[YouTube-Dl]</b>\n' + format_exc(e))
+
+    file = BytesIO(open(f"downloads/{rip_data['id']}.mp4", 'rb').read())
+    file.name = f'{rip_data["id"]}.mp4'
+    file.seek(0)
+
+    await message.reply_video(file, caption=rip_data["title"])
+
+    os.remove(f"downloads/{rip_data['id']}.mp4")
+
+    return await message.delete()
+
+
+modules_help['ytdl'] = {
+    'ytdl [link]': 'Download video by link',
+    'dlyt [link]': 'Download video by link',
+}
