513dcbf1d4f93cc1ed087874483e87b2cae2d1d0
John <60581076+john-phonk@users.noreply.github.com>
2022-04-06 21:10:19 +0300

    black

--- filters.py
+++ filters.py
@@ -9,7 +9,9 @@ from utils.scripts import format_exc
 from utils.db import db
 
 
-all_filters_ = {i["word"]: i["answer"] for i in db.get_collection('lordcodes.filters').values()}
+all_filters_ = {
+    i["word"]: i["answer"] for i in db.get_collection("lordcodes.filters").values()
+}
 
 
 def all_filters():
@@ -34,29 +36,26 @@ async def filter_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 3:
             return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}filter [name] [text] (Reply required)</code>"
-                    )
+                f"<b>Usage</b>: <code>{prefix}filter [name] [text] (Reply required)</code>"
+            )
         name = message.text.split(maxsplit=2)[1]
-        if db.get('lordcodes.filters', f'filter_{name}'):
+        if db.get("lordcodes.filters", f"filter_{name}"):
             return await message.edit(
-                        f"<b>Filter</b> <code>{name}</code> already exists."
-                    )
+                f"<b>Filter</b> <code>{name}</code> already exists."
+            )
         text = message.text.split(name)[1].strip()
         if not message.reply_to_message:
-            return await message.edit('<b>Reply to message</b> please.')
+            return await message.edit("<b>Reply to message</b> please.")
         elif message.reply_to_message.text:
             word = message.reply_to_message.text.lower()
         elif message.reply_to_message.caption:
             word = message.reply_to_message.caption.lower()
         else:
-            return await message.edit('<b>Reply to TEXT/CAPTION message</b> please.')
+            return await message.edit("<b>Reply to TEXT/CAPTION message</b> please.")
 
-        db.set('lordcodes.filters', f'filter_{name}', {'word': word,
-                                                       'answer': text})
+        db.set("lordcodes.filters", f"filter_{name}", {"word": word, "answer": text})
         all_filters_[word] = text
-        return await message.edit(
-                f"<b>Filter</b> <code>{name}</code> has been added."
-            )
+        return await message.edit(f"<b>Filter</b> <code>{name}</code> has been added.")
     except Exception as e:
         return await message.edit(format_exc(e))
 
@@ -64,15 +63,16 @@ async def filter_handler(client: Client, message: Message):
 @Client.on_message(filters.command(["filters"], prefix) & filters.me)
 async def filters_handler(client: Client, message: Message):
     try:
-        text = ''
-        for index, a in enumerate(db.get_collection('lordcodes.filters').items(), start=1):
+        text = ""
+        for index, a in enumerate(
+            db.get_collection("lordcodes.filters").items(), start=1
+        ):
             key, item = a
-            if key[:6] == 'filter':
-                key = key[7:].replace('<', '').replace('>', '')
-                answer = item['answer'].replace('<', '').replace('>', '')
-                text += f"{index}. <code>{key}</code>\n"
-        text = f"<b>Filters</b>:\n\n" \
-               f"{text}"
+            if key[:6] == "filter":
+                key = key[7:].replace("<", "").replace(">", "")
+                answer = item["answer"].replace("<", "").replace(">", "")
+                text += f"{index}. <code>{key}</code> — <code>{answer}</code>\n"
+        text = f"<b>Filters</b>:\n\n" f"{text}"
         text = text[:4096]
         return await message.edit(text)
     except Exception as e:
@@ -83,23 +83,21 @@ async def filters_handler(client: Client, message: Message):
 async def filter_del_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 2:
-            return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}fdel [name]</code>"
-                    )
+            return await message.edit(f"<b>Usage</b>: <code>{prefix}fdel [name]</code>")
         name = message.text.split(maxsplit=1)[1]
-        value = db.get('lordcodes.filters', f'filter_{name}')
+        value = db.get("lordcodes.filters", f"filter_{name}")
         if not value:
             return await message.edit(
-                        f"<b>Filter</b> <code>{name}</code> doesn't exists."
-                    )
+                f"<b>Filter</b> <code>{name}</code> doesn't exists."
+            )
         try:
-            del all_filters_[value['word']]
+            del all_filters_[value["word"]]
         except:
             pass
-        db.remove('lordcodes.filters', f'filter_{name}')
+        db.remove("lordcodes.filters", f"filter_{name}")
         return await message.edit(
-                f"<b>Filter</b> <code>{name}</code> has been deleted."
-            )
+            f"<b>Filter</b> <code>{name}</code> has been deleted."
+        )
     except Exception as e:
         return await message.edit(format_exc(e))
 
@@ -109,22 +107,26 @@ async def filter_search_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 2:
             return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}fsearch [name]</code>"
-                    )
+                f"<b>Usage</b>: <code>{prefix}fsearch [name]</code>"
+            )
         name = message.text.split(maxsplit=1)[1]
-        value = db.get('lordcodes.filters', f'filter_{name}', False)
+        value = db.get("lordcodes.filters", f"filter_{name}", False)
         if not value:
-            return await message.edit(f'<b>Filter</b> <code>{name}</code> doesn\'t exists.')
-        return await message.edit(f'<b>Filter</b> <code>{name}</code>:\n<b>Trigger</b>: <code>{value["word"]}</code'
-                                  f'>\n<b>Answer</b> '
-                                  f': <code>{value["answer"]}</code>')
+            return await message.edit(
+                f"<b>Filter</b> <code>{name}</code> doesn't exists."
+            )
+        return await message.edit(
+            f'<b>Filter</b> <code>{name}</code>:\n<b>Trigger</b>: <code>{value["word"]}</code'
+            f">\n<b>Answer</b> "
+            f': <code>{value["answer"]}</code>'
+        )
     except Exception as e:
         return await message.edit(format_exc(e))
 
 
-modules_help['filters'] = {
-    'filter [name] [text]': 'Create filter (Reply required)',
-    'filters': 'List of all triggers',
-    'fdel [name]': 'Delete filter by name',
-    'fsearch [name]': 'Info filter by name'
+modules_help["filters"] = {
+    "filter [name] [text]": "Create filter (Reply required)",
+    "filters": "List of all triggers",
+    "fdel [name]": "Delete filter by name",
+    "fsearch [name]": "Info filter by name",
 }
