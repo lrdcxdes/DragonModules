55fb38cef42495256444c5afddc98806ef91d727
LORD_CODE <lordgrief176@gmail.com>
2022-04-06 22:05:13 +0300

    fixed modules_help in dice.py

--- filters.py
+++ filters.py
@@ -1,5 +1,11 @@
 from pyrogram import Client, filters, ContinuePropagation, errors
-from pyrogram.types import Message, InputMediaDocument, InputMediaPhoto, InputMediaVideo, InputMediaAudio
+from pyrogram.types import (
+    Message,
+    InputMediaDocument,
+    InputMediaPhoto,
+    InputMediaVideo,
+    InputMediaAudio,
+)
 
 # noinspection PyUnresolvedReferences
 from utils.misc import modules_help, prefix
@@ -9,15 +15,16 @@ from utils.scripts import format_exc
 from utils.db import db
 
 
-all_filters_ =  db.get_collection('lordcodes.filters').items()
+def get_filters_chat(chat_id):
+    return db.get("lordcodes.filters", f"{chat_id}", {})
 
 
-def all_filters():
-    return all_filters_
+def set_filters_chat(chat_id, filters_):
+    return db.set("lordcodes.filters", f"{chat_id}", filters_)
 
 
 async def contains_filter(_, __, m):
-    return m.text and m.text.lower() in all_filters().keys()
+    return m.text and m.text.lower() in get_filters_chat(m.chat.id).keys()
 
 
 contains = filters.create(contains_filter)
@@ -25,11 +32,9 @@ contains = filters.create(contains_filter)
 
 @Client.on_message(contains)
 async def filters_main_handler(client: Client, message: Message):
-    value = all_filters_[message.text.lower()]
+    value = get_filters_chat(message.chat.id)[message.text.lower()]
     try:
-        await client.get_messages(
-            int(value["CHAT_ID"]), int(value["MESSAGE_ID"])
-        )
+        await client.get_messages(int(value["CHAT_ID"]), int(value["MESSAGE_ID"]))
     except errors.RPCError:
         raise ContinuePropagation
 
@@ -62,9 +67,7 @@ async def filters_main_handler(client: Client, message: Message):
                         )
                 elif _.video.thumbs:
                     media_grouped_list.append(
-                        InputMediaVideo(
-                            _.video.file_id, _.video.thumbs[0].file_id
-                        )
+                        InputMediaVideo(_.video.file_id, _.video.thumbs[0].file_id)
                     )
                 else:
                     media_grouped_list.append(InputMediaVideo(_.video.file_id))
@@ -87,9 +90,7 @@ async def filters_main_handler(client: Client, message: Message):
                         )
                     else:
                         media_grouped_list.append(
-                            InputMediaDocument(
-                                _.document.file_id, _.caption.markdown
-                            )
+                            InputMediaDocument(_.document.file_id, _.caption.markdown)
                         )
                 elif _.document.thumbs:
                     media_grouped_list.append(
@@ -98,9 +99,7 @@ async def filters_main_handler(client: Client, message: Message):
                         )
                     )
                 else:
-                    media_grouped_list.append(
-                        InputMediaDocument(_.document.file_id)
-                    )
+                    media_grouped_list.append(InputMediaDocument(_.document.file_id))
         await client.send_media_group(
             message.chat.id,
             media_grouped_list,
@@ -121,15 +120,16 @@ async def filter_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 2:
             return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}filter [name] (Reply required)</code>"
-                    )
+                f"<b>Usage</b>: <code>{prefix}filter [name] (Reply required)</code>"
+            )
         name = message.text.split(maxsplit=1)[1].lower()
-        if db.get('lordcodes.filters', name):
+        chat_filters = get_filters_chat(message.chat.id)
+        if name in chat_filters.keys():
             return await message.edit(
-                        f"<b>Filter</b> <code>{name}</code> already exists."
-                    )
+                f"<b>Filter</b> <code>{name}</code> already exists."
+            )
         if not message.reply_to_message:
-            return await message.edit('<b>Reply to message</b> please.')
+            return await message.edit("<b>Reply to message</b> please.")
 
         try:
             chat = await client.get_chat(db.get("core.notes", "chat_id", 0))
@@ -183,11 +183,10 @@ async def filter_handler(client: Client, message: Message):
                 "CHAT_ID": str(chat_id),
             }
 
-        db.set('lordcodes.filters', name, filter_)
-        all_filters_[name] = filter_
-        return await message.edit(
-                f"<b>Filter</b> <code>{name}</code> has been added."
-            )
+        chat_filters.update({name: filter_})
+
+        set_filters_chat(message.chat.id, chat_filters)
+        return await message.edit(f"<b>Filter</b> <code>{name}</code> has been added.")
     except Exception as e:
         return await message.edit(format_exc(e))
 
@@ -195,13 +194,12 @@ async def filter_handler(client: Client, message: Message):
 @Client.on_message(filters.command(["filters"], prefix) & filters.me)
 async def filters_handler(client: Client, message: Message):
     try:
-        text = ''
-        for index, a in enumerate(db.get_collection('lordcodes.filters').items(), start=1):
+        text = ""
+        for index, a in enumerate(get_filters_chat(message.chat.id).items(), start=1):
             key, item = a
-            key = key.replace('<', '').replace('>', '')
+            key = key.replace("<", "").replace(">", "")
             text += f"{index}. <code>{key}</code>\n"
-        text = f"<b>Filters</b>:\n\n" \
-               f"{text}"
+        text = f"<b>Your filters in current chat</b>:\n\n" f"{text}"
         text = text[:4096]
         return await message.edit(text)
     except Exception as e:
@@ -212,23 +210,18 @@ async def filters_handler(client: Client, message: Message):
 async def filter_del_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 2:
-            return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}fdel [name]</code>"
-                    )
+            return await message.edit(f"<b>Usage</b>: <code>{prefix}fdel [name]</code>")
         name = message.text.split(maxsplit=1)[1].lower()
-        value = db.get('lordcodes.filters', name)
-        if not value:
+        chat_filters = get_filters_chat(message.chat.id)
+        if name not in chat_filters.keys():
             return await message.edit(
-                        f"<b>Filter</b> <code>{name}</code> doesn't exists."
-                    )
-        try:
-            del all_filters_[name]
-        except:
-            pass
-        db.remove('lordcodes.filters', name)
-        return await message.edit(
-                f"<b>Filter</b> <code>{name}</code> has been deleted."
+                f"<b>Filter</b> <code>{name}</code> doesn't exists."
             )
+        del chat_filters[name]
+        set_filters_chat(message.chat.id, chat_filters)
+        return await message.edit(
+            f"<b>Filter</b> <code>{name}</code> has been deleted."
+        )
     except Exception as e:
         return await message.edit(format_exc(e))
 
@@ -238,21 +231,25 @@ async def filter_search_handler(client: Client, message: Message):
     try:
         if len(message.text.split()) < 2:
             return await message.edit(
-                        f"<b>Usage</b>: <code>{prefix}fsearch [name]</code>"
-                    )
+                f"<b>Usage</b>: <code>{prefix}fsearch [name]</code>"
+            )
         name = message.text.split(maxsplit=1)[1].lower()
-        value = db.get('lordcodes.filters', name, False)
-        if not value:
-            return await message.edit(f'<b>Filter</b> <code>{name}</code> doesn\'t exists.')
-        return await message.edit(f'<b>Trigger</b>:\n<code>{name}</code'
-                                  f'>\n<b>Answer</b>:\n{value}')
+        chat_filters = get_filters_chat(message.chat.id)
+        if name not in chat_filters.keys():
+            return await message.edit(
+                f"<b>Filter</b> <code>{name}</code> doesn't exists."
+            )
+        return await message.edit(
+            f"<b>Trigger</b>:\n<code>{name}</code"
+            f">\n<b>Answer</b>:\n{chat_filters[name]}"
+        )
     except Exception as e:
         return await message.edit(format_exc(e))
 
 
-modules_help['filters'] = {
-    'filter [name] [text]': 'Create filter (Reply required)',
-    'filters': 'List of all triggers',
-    'fdel [name]': 'Delete filter by name',
-    'fsearch [name]': 'Info filter by name'
+modules_help["filters"] = {
+    "filter [name]": "Create filter (Reply required)",
+    "filters": "List of all triggers",
+    "fdel [name]": "Delete filter by name",
+    "fsearch [name]": "Info filter by name",
 }
