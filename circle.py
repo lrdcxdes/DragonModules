081bb4c396c1df9c8bc864852ad7e89b45031133
LORD_CODE <lordgrief176@gmail.com>
2022-04-07 22:16:57 +0300

    Update spin.py

--- circle.py
+++ circle.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from io import BytesIO
 
@@ -13,6 +14,42 @@ from utils.scripts import import_library, format_exc
 
 
 VideoFileClip = import_library("moviepy.editor", "moviepy").VideoFileClip
+
+im = None
+
+
+def process_img(filename):
+    global im
+    im = Image.open(f"downloads/{filename}")
+    w, h = im.size
+    img = Image.new("RGBA", (w, h), (0, 0, 0, 0))
+    img.paste(im, (0, 0))
+    m = min(w, h)
+    img = img.crop(((w - m) // 2, (h - m) // 2, (w + m) // 2, (h + m) // 2))
+    w, h = img.size
+    mask = Image.new("L", (w, h), 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((10, 10, w - 10, h - 10), fill=255)
+    mask = mask.filter(ImageFilter.GaussianBlur(2))
+    img = ImageOps.fit(img, (w, h))
+    img.putalpha(mask)
+    im = BytesIO()
+    im.name = "img.webp"
+    img.save(im)
+    im.seek(0)
+
+
+video = None
+
+
+def process_vid(filename):
+    global video
+    video = VideoFileClip(f"downloads/{filename}")
+    video.reader.close()
+    w, h = video.size
+    m = min(w, h)
+    box = [(w - m) // 2, (h - m) // 2, (w + m) // 2, (h + m) // 2]
+    video = video.crop(*box)
 
 
 @Client.on_message(filters.command(["circle", "round"], prefix) & filters.me)
@@ -56,23 +93,7 @@ async def circle(client: Client, message: Message):
         if typ == "photo":
             await message.edit("<b>Processing image</b>📷")
             await message.reply_to_message.download(f"downloads/{filename}")
-            im = Image.open(f"downloads/{filename}")
-            w, h = im.size
-            img = Image.new("RGBA", (w, h), (0, 0, 0, 0))
-            img.paste(im, (0, 0))
-            m = min(w, h)
-            img = img.crop(((w - m) // 2, (h - m) // 2, (w + m) // 2, (h + m) // 2))
-            w, h = img.size
-            mask = Image.new("L", (w, h), 0)
-            draw = ImageDraw.Draw(mask)
-            draw.ellipse((10, 10, w - 10, h - 10), fill=255)
-            mask = mask.filter(ImageFilter.GaussianBlur(2))
-            img = ImageOps.fit(img, (w, h))
-            img.putalpha(mask)
-            im = BytesIO()
-            im.name = "img.webp"
-            img.save(im)
-            im.seek(0)
+            await asyncio.get_event_loop().run_in_executor(None, process_img, filename)
             await message.delete()
             return await message.reply_sticker(
                 sticker=im, reply_to_message_id=message.reply_to_message.message_id
@@ -80,14 +101,11 @@ async def circle(client: Client, message: Message):
         else:
             await message.edit("<b>Processing video</b>🎥")
             await message.reply_to_message.download(f"downloads/{filename}")
-            video = VideoFileClip(f"downloads/{filename}")
-            video.reader.close()
-            w, h = video.size
-            m = min(w, h)
-            box = [(w - m) // 2, (h - m) // 2, (w + m) // 2, (h + m) // 2]
-            video = video.crop(*box)
+            await asyncio.get_event_loop().run_in_executor(None, process_vid, filename)
+
             await message.edit("<b>Saving video</b>📼")
-            video.write_videofile("downloads/result.mp4")
+            await asyncio.get_event_loop().run_in_executor(None, video.write_videofile, "downloads/result.mp4")
+
             await message.delete()
             await message.reply_video_note(
                 video_note="downloads/result.mp4",
