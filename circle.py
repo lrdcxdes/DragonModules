0eece948e722d036346a651de98c9ddd4dcedf75
LORD_CODE <lordgrief176@gmail.com>
2022-04-07 22:04:23 +0300

    Update spin.py

--- circle.py
+++ circle.py
@@ -7,55 +7,56 @@ from pyrogram.types import Message
 
 # noinspection PyUnresolvedReferences
 from utils.misc import modules_help, prefix
+
 # noinspection PyUnresolvedReferences
 from utils.scripts import import_library, format_exc
 
 
-VideoFileClip = import_library('moviepy.editor', 'moviepy').VideoFileClip
+VideoFileClip = import_library("moviepy.editor", "moviepy").VideoFileClip
 
 
 @Client.on_message(filters.command(["circle", "round"], prefix))
 async def circle(client: Client, message: Message):
     try:
         if not message.reply_to_message:
-            return await message.reply('<b>Reply is required for this command</b>')
+            return await message.reply("<b>Reply is required for this command</b>")
         if message.reply_to_message.photo:
-            filename = 'circle.jpg'
-            typ = 'photo'
+            filename = "circle.jpg"
+            typ = "photo"
         elif message.reply_to_message.sticker:
             if message.reply_to_message.sticker.is_video:
-                return await message.reply('<b>Video stickers is not supported</b>')
-            filename = 'circle.webp'
-            typ = 'photo'
+                return await message.reply("<b>Video stickers is not supported</b>")
+            filename = "circle.webp"
+            typ = "photo"
         elif message.reply_to_message.video:
-            filename = 'circle.mp4'
-            typ = 'video'
+            filename = "circle.mp4"
+            typ = "video"
         elif message.reply_to_message.document:
             _filename = message.reply_to_message.document.file_name.casefold()
-            if _filename.endswith('.png'):
-                filename = 'circle.png'
-                typ = 'photo'
-            elif _filename.endswith('.jpg'):
-                filename = 'circle.jpg'
-                typ = 'photo'
-            elif _filename.endswith('.jpeg'):
-                filename = 'circle.jpeg'
-                typ = 'photo'
-            elif _filename.endswith('.webp'):
-                filename = 'circle.webp'
-                typ = 'photo'
-            elif _filename.endswith('.mp4'):
-                filename = 'circle.mp4'
-                typ = 'video'
+            if _filename.endswith(".png"):
+                filename = "circle.png"
+                typ = "photo"
+            elif _filename.endswith(".jpg"):
+                filename = "circle.jpg"
+                typ = "photo"
+            elif _filename.endswith(".jpeg"):
+                filename = "circle.jpeg"
+                typ = "photo"
+            elif _filename.endswith(".webp"):
+                filename = "circle.webp"
+                typ = "photo"
+            elif _filename.endswith(".mp4"):
+                filename = "circle.mp4"
+                typ = "video"
             else:
-                return await message.reply('<b>Invalid file type</b>')
+                return await message.reply("<b>Invalid file type</b>")
         else:
-            return await message.reply('<b>Invalid file type</b>')
+            return await message.reply("<b>Invalid file type</b>")
 
-        if typ == 'photo':
+        if typ == "photo":
             await message.edit("<b>Processing image</b>📷")
-            await message.reply_to_message.download(f'downloads/{filename}')
-            im = Image.open(f'downloads/{filename}')
+            await message.reply_to_message.download(f"downloads/{filename}")
+            im = Image.open(f"downloads/{filename}")
             w, h = im.size
             img = Image.new("RGBA", (w, h), (0, 0, 0, 0))
             img.paste(im, (0, 0))
@@ -73,12 +74,13 @@ async def circle(client: Client, message: Message):
             img.save(im)
             im.seek(0)
             await message.delete()
-            return await message.reply_sticker(sticker=im,
-                                               reply_to_message_id=message.reply_to_message.message_id)
+            return await message.reply_sticker(
+                sticker=im, reply_to_message_id=message.reply_to_message.message_id
+            )
         else:
             await message.edit("<b>Processing video</b>🎥")
-            await message.reply_to_message.download(f'downloads/{filename}')
-            video = VideoFileClip(f'downloads/{filename}')
+            await message.reply_to_message.download(f"downloads/{filename}")
+            video = VideoFileClip(f"downloads/{filename}")
             video.reader.close()
             w, h = video.size
             m = min(w, h)
@@ -87,17 +89,15 @@ async def circle(client: Client, message: Message):
             await message.edit("<b>Saving video</b>📼")
             video.write_videofile("downloads/result.mp4")
             await message.delete()
-            await message.reply_video_note(video_note="downloads/result.mp4",
-                                           duration=int(video.duration),
-                                           reply_to_message_id=message.reply_to_message.message_id)
+            await message.reply_video_note(
+                video_note="downloads/result.mp4",
+                duration=int(video.duration),
+                reply_to_message_id=message.reply_to_message.message_id,
+            )
             os.remove(f"downloads/{filename}")
             os.remove("downloads/result.mp4")
     except Exception as e:
-        await message.reply(
-            format_exc(e)
-        )
+        await message.reply(format_exc(e))
 
 
-modules_help['circle'] = {
-    'round': 'Закруглить фото или видео.'
-}
+modules_help["circle"] = {"round": "Закруглить фото или видео."}
